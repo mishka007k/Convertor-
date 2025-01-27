e5c0576b8d06f7c9130776ba221c1d505786a872
Mikhail Kazadaev <misha007K@inbox.ru>
2025-01-27 20:52:55 +0300

    Добавлена новая функция приложения!

---
+++ NewFunction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convertor
+{
+    public class NewFunction
+    {
+        public static void function()
+        {
+            // Ккакой-то функционал приложения
+        }
+
+    }
+}
